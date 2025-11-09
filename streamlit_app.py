281b45a79702799d9083e1a39ecd67eb507975d2
Minhtri-hub <nguyenquachminhtri@gmail.com>
2025-11-09 22:08:26 +0700

    Change image size from 768x768 to 1024x1024

--- streamlit_app.py
+++ streamlit_app.py
@@ -314,7 +314,7 @@ def generate_images(scenes: List[Scene], openai_client: Optional[OpenAI]) -> Non
             response = openai_client.images.generate(
                 model=OPENAI_IMAGE_MODEL,
                 prompt=scene.prompt,
-                size="768x768",
+                size="1024x1024",  
             )
             image_base64 = response.data[0].b64_json
             image_bytes = base64.b64decode(image_base64)
